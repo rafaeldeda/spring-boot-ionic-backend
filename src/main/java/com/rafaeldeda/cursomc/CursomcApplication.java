4b1d6e4cdb88f18abb04c1c0cd0cfcce826dc9f0
Rafael Deda <rafael.deda@gmail.com>
2020-05-24 21:31:42 -0300

    criação de cliente tipocliente endereco telefones

--- src/main/java/com/rafaeldeda/cursomc/CursomcApplication.java
+++ src/main/java/com/rafaeldeda/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rafaeldeda.cursomc.domain.Categoria;
 import com.rafaeldeda.cursomc.domain.Cidade;
+import com.rafaeldeda.cursomc.domain.Cliente;
+import com.rafaeldeda.cursomc.domain.Endereco;
 import com.rafaeldeda.cursomc.domain.Estado;
 import com.rafaeldeda.cursomc.domain.Produto;
+import com.rafaeldeda.cursomc.domain.enums.TipoCliente;
 import com.rafaeldeda.cursomc.repositories.CategoriaRepository;
 import com.rafaeldeda.cursomc.repositories.CidadeRepository;
+import com.rafaeldeda.cursomc.repositories.ClienteRepository;
+import com.rafaeldeda.cursomc.repositories.EnderecoRepository;
 import com.rafaeldeda.cursomc.repositories.EstadoRepository;
 import com.rafaeldeda.cursomc.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -59,11 +68,22 @@ public class CursomcApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 		
+		Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","009.257.385-14",TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("999988889","32312540"));
+		
+		Endereco e1 = new Endereco(null,"Rua Flores","123","CASA", "Grageru", "49015110", cli1, c1);
+		Endereco e2 = new Endereco(null,"Avenida Matos","321","COMERCIO", "Centro", "88777000", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 		
 	}
 
---
+++ src/main/java/com/rafaeldeda/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.rafaeldeda.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafaeldeda.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
