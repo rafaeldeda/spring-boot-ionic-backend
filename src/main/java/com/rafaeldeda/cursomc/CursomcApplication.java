513256458264ff65b4359c4492fcc8e8516f1a52
Rafael Deda <rafael.deda@gmail.com>
2020-05-24 22:34:16 -0300

    criação de pedido pagamento

--- src/main/java/com/rafaeldeda/cursomc/CursomcApplication.java
+++ src/main/java/com/rafaeldeda/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.rafaeldeda.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.rafaeldeda.cursomc.domain.Cidade;
 import com.rafaeldeda.cursomc.domain.Cliente;
 import com.rafaeldeda.cursomc.domain.Endereco;
 import com.rafaeldeda.cursomc.domain.Estado;
+import com.rafaeldeda.cursomc.domain.Pagamento;
+import com.rafaeldeda.cursomc.domain.PagamentoComBoleto;
+import com.rafaeldeda.cursomc.domain.PagamentoComCartao;
+import com.rafaeldeda.cursomc.domain.Pedido;
 import com.rafaeldeda.cursomc.domain.Produto;
+import com.rafaeldeda.cursomc.domain.enums.EstadoPagamento;
 import com.rafaeldeda.cursomc.domain.enums.TipoCliente;
 import com.rafaeldeda.cursomc.repositories.CategoriaRepository;
 import com.rafaeldeda.cursomc.repositories.CidadeRepository;
 import com.rafaeldeda.cursomc.repositories.ClienteRepository;
 import com.rafaeldeda.cursomc.repositories.EnderecoRepository;
 import com.rafaeldeda.cursomc.repositories.EstadoRepository;
+import com.rafaeldeda.cursomc.repositories.PagamentoRepository;
+import com.rafaeldeda.cursomc.repositories.PedidoRepository;
 import com.rafaeldeda.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -59,32 +71,47 @@ public class CursomcApplication implements CommandLineRunner {
 
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
+
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
-		
+
 		est1.getCidades().addAll(Arrays.asList(c1));
-		est2.getCidades().addAll(Arrays.asList(c2,c3));
-		
-		Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","009.257.385-14",TipoCliente.PESSOAFISICA);
-		cli1.getTelefones().addAll(Arrays.asList("999988889","32312540"));
-		
-		Endereco e1 = new Endereco(null,"Rua Flores","123","CASA", "Grageru", "49015110", cli1, c1);
-		Endereco e2 = new Endereco(null,"Avenida Matos","321","COMERCIO", "Centro", "88777000", cli1, c2);
-		
-		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
-		
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "009.257.385-14", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("999988889", "32312540"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "123", "CASA", "Grageru", "49015110", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "321", "COMERCIO", "Centro", "88777000", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 11:30"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
-		estadoRepository.saveAll(Arrays.asList(est1,est2));
-		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
-		
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
 		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1,e2));
-		
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
 	}
 
 }
--- src/main/java/com/rafaeldeda/cursomc/domain/Cliente.java
+++ src/main/java/com/rafaeldeda/cursomc/domain/Cliente.java
@@ -38,6 +38,9 @@ public class Cliente implements Serializable{
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<String>();
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<Pedido>();
 
 	public Cliente() {
 	}
@@ -105,6 +108,14 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -130,6 +141,8 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
+
+	
 	
 	
 }
