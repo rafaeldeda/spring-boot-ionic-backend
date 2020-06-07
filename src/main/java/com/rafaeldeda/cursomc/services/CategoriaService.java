dcb37e25a60648a2a7c685ca679e57471b36d038
Rafael Deda <rafael.deda@gmail.com>
2020-06-07 19:43:28 -0300

    listando categorias com dto

--- src/main/java/com/rafaeldeda/cursomc/services/CategoriaService.java
+++ src/main/java/com/rafaeldeda/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.rafaeldeda.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,30 +17,34 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
-		repo.deleteById(id);
-		}catch(DataIntegrityViolationException e) {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
 	}
-	
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+
 }
