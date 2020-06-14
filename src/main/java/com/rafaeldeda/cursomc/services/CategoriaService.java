7a27ef70f4c02324734472ba6d960a056d98397b
Rafael Deda <rafael.deda@gmail.com>
2020-06-14 11:31:17 -0300

    paginação com parâmetros opcionais na requisição

--- src/main/java/com/rafaeldeda/cursomc/services/CategoriaService.java
+++ src/main/java/com/rafaeldeda/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rafaeldeda.cursomc.domain.Categoria;
+import com.rafaeldeda.cursomc.dto.CategoriaDTO;
 import com.rafaeldeda.cursomc.repositories.CategoriaRepository;
 import com.rafaeldeda.cursomc.services.exception.DataIntegrityException;
 import com.rafaeldeda.cursomc.services.exception.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(),objDTO.getNome());
 	}
 
 }
